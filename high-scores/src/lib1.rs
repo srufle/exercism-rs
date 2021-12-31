ed2228f173907a859c6231779bc6d84214d0ad1d
Stephen Rufle <srufle@concordservicing.com>
2021-12-31 05:54:25 -0700

    Solved high-scores exercise

--- high-scores/src/lib1.rs
+++ high-scores/src/lib1.rs
@@ -3,8 +3,6 @@ pub struct HighScores<'a> {
     scores: &'a [u32],
 }
 
-// Insired by
-// https://exercism.org/tracks/rust/exercises/high-scores/solutions/menb111
 impl<'a> HighScores<'a> {
     pub fn new(scores: &'a [u32]) -> Self {
         HighScores { scores }
@@ -15,19 +13,31 @@ impl<'a> HighScores<'a> {
     }
 
     pub fn latest(&self) -> Option<u32> {
-        // replaced match with *cloned*, I had previously tried clone
-        self.scores.last().cloned()
+        match self.scores.last() {
+            Some(x) => Some(*x),
+            None => None,
+        }
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        // Was using a for loop to do what I think max does
-        // in the iterator
-        self.scores.iter().max().cloned()
+        if self.scores.is_empty() {
+            return None;
+        }
+        let mut best = 0;
+        for score in self.scores {
+            if score >= &best {
+                best = *score
+            }
+        }
+
+        Some(best)
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        let mut top_three = self.scores.to_vec();
-
+        let mut top_three: Vec<u32> = Vec::new();
+        for score in self.scores {
+            top_three.push(*score);
+        }
         // Sort descending
         top_three.sort_by(|a, b| b.cmp(a));
         top_three.truncate(3);
